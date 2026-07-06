9ca33ea6d22e50caefe0b3dd3e71dd5eff6449a7
euadorj <benwee496@gmail.com>
2026-07-06 13:51:50 +0800

    oops forgot tab4 needs to be standalone. as of now tab 4 has no effect when it changes tab but will fix and update later. hope to send chatgpt css soon for template soon.

--- src/app/tabs/BENEDICT/tab4.page.ts
+++ src/app/tabs/BENEDICT/tab4.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
+  standalone: false
 })
 export class Tab4Page implements OnInit {
 
